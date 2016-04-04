c0321e9c277d1e00c0cf9cca6d22bcc2259a0c39
linx214 <linx214@gmail.com>
2016-04-04 22:35:54 +0800

    add proj

---
+++ LXSlider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LXSlider
+//
+//  Created by linx on 16/4/3.
+//  Copyright © 2016年 linx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
